f4eae783119d94c4aa848261207889cf2b25a725
Ibad Ur Rehman <HAFIZIBADURREHMAN363@GMAIL.COM>
2024-10-17 18:05:00 +0500

    page 1 working

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,46 +3,22 @@ import Link from 'next/link';
 
 const Header = () => {
   return (
-    // <header className="bg-[#044e83] text-white p-2">
-    //   <div className="container mx-auto flex justify-around items-center">
-    //     {/* Logo */}
-    //     <div className="flex items-center">
-    //     <img src="/assets/logo.png" alt="Logo" className="h-24 w-20" />
-    //     </div>
-    //       <h1 className="text-xl font-semibold text-[#b9d8f3]">Tuition Free Education Program on Latest Technologies</h1>
-
-    //     {/* Navigation */}
-    //     <nav>
-    //       <ul className="flex space-x-8">
-    //         <li><Link href="/"  className="hover:text-gray-300">Home</Link></li>
-    //         <li><Link href="/" className="hover:text-gray-300"> Apply</Link></li>
-    //         <li><Link href="/" className="hover:text-gray-300">Jobs</Link></li>
-    //         <li><Link href="/" className="hover:text-gray-300">Result</Link></li>
-    //         <li className="relative group">
-    //           <Link href="" className="hover:text-gray-300">Courses</Link>
-    //           {/* Dropdown */}
-    //           <ul className="absolute hidden group-hover:block bg-blue-900 mt-2 rounded-md py-2 shadow-lg">
-    //             <li><Link href="/" className="block px-4 py-2 hover:bg-[#044e83]">Course 1</Link></li><hr />
-    //             <li><Link href="/" className="block px-4 py-2 hover:bg-[#044e83]">Course 2</Link></li><hr />
-    //             <li><Link href="/" className="block px-4 py-2 hover:bg-[#044e83]">Course 3</Link></li>
-    //           </ul>
-    //         </li>
-    //       </ul>
-    //     </nav>
-    //   </div>
-    // </header>
-
-    // copy of header for practice
-    <header className='relative flex justify-around items-center bg-[#044e83] text-white w-full h-20 max-md:text-black'>
-        <div><img src="/assets/logo.png" alt="Logo" className='w-24 h-30 absolute left-10 top-4'/></div>
+    <header className='fixed flex top-0 left-0 z-50 justify-around items-center bg-[#044e83] text-white w-full h-20 p-2 max-md:flex-col'>
+        <div><img src="/assets/logo.png" alt="Logo" className='w-24 h-30 absolute left-8 top-3 max-md:top-0'/></div>
         <h1 className='font-bold text-2xl text-[#b9d8f3]'>Tuition Free Education Program on Latest Technologies</h1>
         <nav>
-          <ul className='flex space-x-9 '>
-            <Link href="/"><li className="hover:text-gray-300">Home</li></Link>
-            <Link href="/"><li className="hover:text-gray-300">Apply</li></Link>
-            <Link href="/"><li className="hover:text-gray-300">Jobs</li></Link>
-            <Link href="/"><li className="hover:text-gray-300">Result</li></Link>
-            <Link href="/"><li className="hover:text-gray-300">Courses</li></Link>
+          <ul className='flex space-x-9'>
+            <li className='hover:text-gray-300'><Link href='/'>Home</Link></li>
+            <li className='hover:text-gray-300'><Link href='/'>Apply</Link></li>
+            <li className='hover:text-gray-300'><Link href='/'>Jobs</Link></li>
+            <li className='hover:text-gray-300'><Link href='/'>Result</Link></li>
+            <li className='relative group hover:text-gray-300'><span>Courses</span>
+              <ul className='absolute hidden group-hover:block bg-[#044e83] text-white mt-0.5 rounded-md shadow-lg'>
+                <li><Link href='/' className='block px-4 py-2 text-center'>Artificial Intelligence</Link></li><hr />
+                <li><Link href='/' className='block px-4 py-2'>Web 3.0</Link></li><hr />
+                <li><Link href='/' className='block px-4 py-2'>Metaverse</Link></li>
+              </ul>
+            </li>
           </ul>
         </nav>
     </header>
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import Header from "./components/Header";
+import Page1 from "./components/Page1";
 
 export default function Home() {
   return (
     <>
     <Header/>
-    <br /><br />
-    lorem1500
+    
+    <Page1/>
     </>
   );
 }
